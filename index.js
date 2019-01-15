8a88df366bd9efb067a6beec7490d890a1091d21
ColeWoodford <woodford.cole@gmail.com>
2019-01-14 19:19:58 -0800

    Surely this time it will work

--- index.js
+++ index.js
@@ -10,13 +10,20 @@ app.use(bodyParser.json())
  * For CRUD using sequelize:
  * @see https://lorenstewart.me/2016/10/03/sequelize-crud-101/
  */
+if (process.env.NODE_ENV == 'production') {
+	app.use(express.static(path.resolve(__dirname, '../client.build')));
 
-app.use((req, res, next) => {
-	res.header('Content-Type', 'application/json');
-	next();
-});
+	app.get('/*', (request, response) => {
+		response.sendFile(path.resolve(__dirname, '../client/build/index.html'));
+	})
+} else {
+	app.use((req, res, next) => {
+		res.header('Content-Type', 'application/json');
+		next();
+	});
+}
 
-	router(app, db);
+router(app, db);
 
 const PORT = process.env.PORT || 8000;
 app.set('port', PORT);
