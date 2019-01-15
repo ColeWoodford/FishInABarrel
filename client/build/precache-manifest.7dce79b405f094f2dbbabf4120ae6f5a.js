a17d2ecab5f70f829df8d0fc031489fbc4594fe1
ColeWoodford <woodford.cole@gmail.com>
2019-01-15 09:33:54 -0800

    Removed build from ignore.

---
+++ client/build/precache-manifest.7dce79b405f094f2dbbabf4120ae6f5a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "7d827127ed3c4a3c77eb",
+    "url": "/static/js/main.7d827127.chunk.js"
+  },
+  {
+    "revision": "7935d4d80a7b3cd0ea2e",
+    "url": "/static/js/1.7935d4d8.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "cb790da8bcf6cac55cce7f222ee70faf",
+    "url": "/index.html"
+  }
+];
