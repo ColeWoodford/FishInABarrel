b0dc959c17f52c13560a0ff33315f76f5001c77f
ColeWoodford <woodford.cole@gmail.com>
2019-01-15 08:45:36 -0800

    Another path was wrong.

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ if (process.env.NODE_ENV == 'production') {
 	app.use(express.static(path.resolve(__dirname, './client')));
 
 	app.get('/*', (request, response) => {
-		response.sendFile(path.resolve(__dirname, './client/index.js'));
+		response.sendFile(path.resolve(__dirname, './client/src/index.js'));
 	})
 } else {
 	app.use((req, res, next) => {
