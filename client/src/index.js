4d64f61b1e742ac906b59748acf884a1c2c08b79
ColeWoodford <woodford.cole@gmail.com>
2019-01-23 18:31:07 -0800

    https*

--- client/src/index.js
+++ client/src/index.js
@@ -19,7 +19,7 @@ const store = createStore(
 	applyMiddleware(sagaMiddleware)
 );
 
-const socket = io('http://secure-bastion-35148.herokuapp.com/', { transport : ['websocket'] });
+const socket = io('https://secure-bastion-35148.herokuapp.com/', { transport : ['websocket'] });
 
 // then run the saga
 sagaMiddleware.run(mySaga);
